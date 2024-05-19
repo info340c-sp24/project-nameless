7861cee51cc160c12bdc9d2a8f292b14abdaf4d0
OxO-106 <oxo106@uw.edu>
2024-05-18 17:23:35 -0700

    login signup done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../style/footer.css'
+import '../style/general.css'
 
 const Footer = () => {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../style/header.css'
+import '../style/general.css'
 
 const Header = () => {
   return (
